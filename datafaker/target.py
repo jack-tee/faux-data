9ce8ddc946cccefc6fc9cf825470631b3275c5b6
jack-tee <jetaylor0@gmail.com>
2021-12-01 20:09:45 +0000

    add basic target

--- datafaker/target.py
+++ datafaker/target.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass, field
 from typing import List
 
+from .template_entity import TemplateEntity
+
 
 @dataclass(kw_only=True)
-class Target:
-    type: str
+class Target(TemplateEntity):
+    type_key: str = field(default="target", init=False)
+    short_key: str = field(default="t", init=False)
+    short_skip_fields: str = field(default="", init=False)
+    import_path: str | None = field(default="", init=False)
+
+    target: str
 
 
 @dataclass(kw_only=True)
