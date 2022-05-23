68cc6bed713ab5d139de6926612289801951e2d3
jack-tee <10283360+jack-tee@users.noreply.github.com>
2022-05-23 15:36:29 +0100

    initial working cloud function

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,20 +1,24 @@
 from faux_data.template import Template
 
+import logging
+
+logging.basicConfig(level='DEBUG')
+
 
 def generate(request):
     """Entry point for the Google Cloud Function."""
 
-    # request_json = request.get_json()
-    if not request.args:
-        raise ValueError("no request args")
+    request_json = request.get_json()
+    # if not request.args:
+    #     raise ValueError("no request args")
 
-    if "template" in request.args:
-        # passing a template path
-        # read file from cloud storage and initialise template
-        t = Template.from_file(request.args["template"])
+    logging.info(request_json)
 
-    elif "template_str" in request.args:
-        t = Template.from_base64_string(request.args["template_str"])
+    if "template" in request_json:
+        t = Template.from_file(request_json["template"], params=request_json)
+
+    elif "template_str" in request_json:
+        t = Template.from_base64_string(request_json["template_str"])
 
     else:
         raise ValueError(
@@ -22,4 +26,5 @@ def generate(request):
         )
 
     t.run()
-    return t.result()
+    logging.info(t)
+    return "done"
